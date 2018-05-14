dc2ef09e7c3f433fca668953384cba59d2796ee7
gengxin1995 <525038358@qq.com>
2018-05-14 13:25:50 +0800

    update

--- algorithm/Data Structure/Binary Tree/BinaryTree.js
+++ algorithm/Data Structure/Binary Tree/BinaryTree.js
@@ -107,8 +107,69 @@ BST.prototype.find = function (val) {
         }
     }
 };
+//返回最小值
+BST.prototype.min = function (node) {
+    var cur = node ? node : this.root;
+    if (cur) {
+        while (cur && cur.left !== null) {
+            cur = cur.left;
+        }
+        return cur.val;
+    }
+    return null;
+};
+//返回最大值
+BST.prototype.max = function (node) {
+    var cur = node ? node : this.root;
+    if (cur) {
+        while (cur && cur.right !== null) {
+            cur = cur.right;
+        }
+        return cur.val;
+    }
+    return null;
+};
+//移除一个节点
+BST.prototype.remove = function (val) {
+    this.root = removeNode(this.root, val);
+};
 
-
+function removeNode(node, val) {
+    if (node === null)
+        return node;
+    if (val < node.val) {
+        node.left = removeNode(node.left, val);
+        return node;
+    } else if (val > node.val) {
+        node.right = removeNode(node.right, val);
+        return node;
+    } else {
+        //叶子节点
+        if (node.left === null && node.right === null) {
+            node = null;
+            return node;
+        }
+        //有一个子节点的节点
+        if (node.left === null) {
+            node = node.right;
+            return node;
+        } else if (node.right === null) {
+            node = node.left;
+            return node;
+        }
+        //有两个子节点的节点
+        var pos = findMinNode(node.right);
+        node.val = pos;
+        node.right = removeNode(node.right, pos);
+        return node;
+    }
+}
+function findMinNode(node) {
+    while (node && node.left !==null) {
+        node = node.left;
+    }
+    return node.val;
+}
 var arr = [18, 12, 3, 14, 25, 7, 22, 28];
 var bst = new BST();
 for (var i = 0; i < arr.length; i++) {
@@ -116,13 +177,21 @@ for (var i = 0; i < arr.length; i++) {
 }
 bst.inOrder(bst.root);
 console.log(bst.traverse);
-bst.preOrder(bst.root);
-console.log(bst.traverse);
-bst.postOrder(bst.root);
-console.log(bst.traverse);
-bst.bfs(bst.root);
-console.log(bst.traverse);
+// bst.preOrder(bst.root);
+// console.log(bst.traverse);
+// bst.postOrder(bst.root);
+// console.log(bst.traverse);
+// bst.bfs(bst.root);
+// console.log(bst.traverse);
 //console.log(bst.find(7) ? true : false);
+// console.log(bst.min());
+// console.log(bst.max());
+bst.remove(18)
+bst.inOrder(bst.root);
+console.log(bst.traverse);
+
+
+
 
 // var bst1 = new BST();
 // bst1.inOrder(bst1.root);
