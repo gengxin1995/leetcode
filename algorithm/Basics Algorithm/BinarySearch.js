5c86b48c74909365af90968d63ebce1a7e5f0b7f
gengxin1995 <525038358@qq.com>
2018-05-17 11:20:18 +0800

    binary search

---
+++ algorithm/Basics Algorithm/BinarySearch.js
@@ -0,0 +1,24 @@
+/**
+ * 输入为排好序的数组
+ * @param arr
+ * @param target
+ * @returns {number}
+ */
+function binarySearch(arr, target) {
+    var left = 0,
+        right = arr.length - 1;
+    while (left <= right) {
+        var mid = Math.floor((left + right) / 2);
+        if (arr[mid] > target) {
+            right = mid - 1;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+var arr = [1,2,3,4,5,6,7,8];
+console.log(binarySearch(arr, 5));
