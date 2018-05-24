cb3dc33aba3e303dea4cf09dc5ed210a8636ddb3
gengxin1995 <525038358@qq.com>
2018-05-24 11:05:48 +0800

    add

---
+++ algorithm/String/Space Replacement.js
@@ -0,0 +1,21 @@
+/**
+ * Write a method to replace all spaces in a string with %20 . The string is given in a characters array, you can assume it has enough space for replacement and you are given the true length of the string.
+ * You code should also return the new length of the string after replacement.
+ *
+ * Notice
+ * If you are using Java or Python， please use characters array instead of string.
+ *
+ * Example
+ * Given "Mr John Smith" , length = 13 .
+ * The string after replacement should be "Mr%20John%20Smith" , you need to change the string in-place and return the new length 17 .
+ *
+ * Challenge
+ * Do it in-place.
+ */
+var spaceReplacement = function (s) {
+    s = s.replace(/\s+/g, '%20');
+    return s.length;
+};
+
+var s = "Mr John Smith";
+console.log(spaceReplacement(s));
