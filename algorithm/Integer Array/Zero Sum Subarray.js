6dbcd083f17f67bedb4704e457da687f1d27f2d8
gengxin1995 <525038358@qq.com>
2018-05-28 20:36:16 +0800

    add

---
+++ algorithm/Integer Array/Zero Sum Subarray.js
@@ -0,0 +1,26 @@
+/**
+ * @param nums: A list of integers
+ * @return: A list of integers includes the index of the first number and the index of the last number
+ */
+const subarraySum = function (nums) {
+    var res = [];
+    var hash = {};
+    hash[0] = 0;
+    var sum = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+        //累加数组中的值
+        sum += nums[i];
+        if (hash.hasOwnProperty(sum)) {
+            //如果索引i处的累加和对应的hash值已存在，则表示从该hash值到索引i的子数组和为0
+            res.push(hash[sum]);
+            res.push(i);
+            return res;
+        } else {
+            hash[sum] = i + 1;
+        }
+    }
+    return res;
+};
+var nums = [4,2,-3,1,6];
+console.log(subarraySum(nums));
