71856f93d48da708f96c0bbc9b4dd7627c0284ff
gengxin1995 <525038358@qq.com>
2018-07-11 23:29:01 +0800

    add

---
+++ algorithm/9_Binary Tree/104_Maximum Depth of Binary Tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (root == null) {
+        return 0;
+    }
+
+    var left_depth = maxDepth(root.left);
+    var right_depth = maxDepth(root.right);
+
+    return Math.max(left_depth, right_depth) + 1;
+};
